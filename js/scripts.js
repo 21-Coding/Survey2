a441d87e367510bff08eef2e36814bae3d1f1317
DominiqueYoumans <ddyoumans@icloud.com>
2019-10-25 09:21:27 -0700

    add results in js

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,14 @@ $(document).ready(function(){
     event.preventDefault();
 
     var name = $("#username").val();
+    var holiday = parseInt($("#holiday").val());
+    var color = parseInt($("#color").val());
+    var movieGenre = parseInt($("#movieGenre").val());
+    var weather = parseInt($("#weather").val());
+    var candy = parseInt($("#candy").val());
+
+    var results = holiday + color + movieGenre + weather + candy;
+    console.log(results);
+
+    
   });
