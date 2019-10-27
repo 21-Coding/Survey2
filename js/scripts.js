6ef2c5c626c879a7740966a2da75a58a4025eeca
DominiqueYoumans <ddyoumans@icloud.com>
2019-10-27 09:48:43 -0700

    add more branching

--- js/scripts.js
+++ js/scripts.js
@@ -2,31 +2,22 @@ $(document).ready(function(event){
   $("form#form").submit(function(event){
     event.preventDefault();
     var name = $("#username").val();
+    var gender = $("#gender").val();
     var holiday = parseInt($("#holiday").val());
     var color = parseInt($("#color").val());
     var movieGenre = parseInt($("#movieGenre").val());
     var weather = parseInt($("#weather").val());
     var candy = parseInt($("#candy").val());
 
-    var results = holiday + color + movieGenre + weather + candy;
-    console.log(results);
 
-    if (results >= 8 && results <= 12 ){
+
+
+    if (gender === "male" && gender === "female" && gender === "undecided"){
       $(".go").fadeIn();
       $(".hide-form").hide();
       $("body").removeClass();
     $("body").addClass("go-class");
-    } else if (results >= 12 && results <= 25){
-      $(".python").fadeIn();
-      $(".hide-form").hide();
-      $("body").removeClass();
-    $("body").addClass("python-class");
-    } else if (results >= 26){
-      $(".rust").fadeIn();
-      $(".hide-form").hide();
-      $("body").removeClass();
-    $("body").addClass("rust-class");
-    }
+    
 
     $(".text").text(name);
 
