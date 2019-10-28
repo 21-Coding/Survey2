0efdfa54f3cba3f6d744aeaa7618b4667a10bd81
DominiqueYoumans <ddyoumans@icloud.com>
2019-10-27 20:03:59 -0700

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,27 @@ $(document).ready(function(event){
   $("form#form").submit(function(event){
     event.preventDefault();
     var name = $("#username").val();
-    var gender = $("#gender").val();
+    var gender = parseInt($("#gender").val());
     var holiday = parseInt($("#holiday").val());
     var color = parseInt($("#color").val());
     var movieGenre = parseInt($("#movieGenre").val());
     var weather = parseInt($("#weather").val());
     var candy = parseInt($("#candy").val());
 
+    var results = gender;
+    
 
 
 
-    if (gender === "male" && gender === "female" && gender === "undecided"){
+    if (gender == "male" && gender == "female" && gender == "undecided"){
       $(".go").fadeIn();
       $(".hide-form").hide();
       $("body").removeClass();
     $("body").addClass("go-class");
-    
+
 
     $(".text").text(name);
 
-
+    };
   });
 });
